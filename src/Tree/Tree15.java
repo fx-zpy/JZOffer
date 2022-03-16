b6bab77769ce104ec1c5f5f4b86a00056587e9af
fx-zpy <2248171510@qq.com>
2022-03-16 08:30:16 +0800

    tree

--- src/Tree/Tree15.java
+++ src/Tree/Tree15.java
@@ -1,10 +1,48 @@
 package Tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+
 /**
  * JZ78 把二叉树打印成多行
  */
 
 public class Tree15 {
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
 
+        ArrayList<ArrayList<Integer>> arrs=new ArrayList<>();
+        HashMap<TreeNode,Integer> hashMap=new HashMap<>();
+        if(pRoot==null){
+            return arrs;
+        }
+        Deque<TreeNode> deque=new ArrayDeque<>();
+        deque.addFirst(pRoot);
+        hashMap.put(pRoot,0);
+        while(!deque.isEmpty()){
+            pRoot=deque.pollLast();
+            int deep=hashMap.get(pRoot);
+            if(pRoot.left!=null){
+                deque.addFirst(pRoot.left);
+                hashMap.put(pRoot.left,deep+1);
+            }
+            if(pRoot.right!=null){
+                deque.addFirst(pRoot.right);
+                hashMap.put(pRoot.right,deep+1);
+            }
+            if(arrs.size()<=deep){
+                ArrayList<Integer> arr=new ArrayList<>();
+                arr.add(pRoot.val);
+                arrs.add(arr);
+            }
+            else{
+                ArrayList<Integer> arr=arrs.get(deep);
+                arr.add(pRoot.val);
+            }
+
+        }
+        return arrs;
+    }
 
 }
